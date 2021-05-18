6e287bbedcb4d80abeae3116eff4e4d03cfa774b
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-18 10:39:02 -0300

    Lista 3

---
+++ Problemas Antigos UERJ/Lista3-Prog1/Lista3_Ex7.c
@@ -0,0 +1,15 @@
+/* @jxvtrl */
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+ printf("Hello World\n"); // Faz com que apos a frase seja lida pelo sistema, ele de um enter
+ 
+ printf("Hello\nWorld\n"); // Pula a linha apos o 'Hello' e apos 'World'
+ 
+ printf("Hello \\ World\n"); // Pula a linha apos a leitura e adiciona uma barra no meio da frase
+ 
+ printf("\"Hello World\"\n"); // adiciona aspas na frase e pula a proxima linha
+ 
+return 0;
+}
