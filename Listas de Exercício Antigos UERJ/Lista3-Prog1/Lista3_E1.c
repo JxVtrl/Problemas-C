8aa26d5874846d0b549596daf6354a071d091446
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2022-02-03 16:59:24 -0300

    Pequeno update

--- Listas de Exercício Antigos UERJ/Lista3-Prog1/Lista3_E1.c
+++ Listas de Exercício Antigos UERJ/Lista3-Prog1/Lista3_E1.c
@@ -1,15 +1,21 @@
 /* @jxvtrl */
 
 #include <stdio.h>
+
 int main (){
-int a,b;
-printf ("Entre com a: \n");
-scanf ("%d", &a);
+    int a,b;
+    printf ("Entre com a: \n");
+    scanf ("%d", &a);
 
-printf ("Entre com b: \n");
-scanf ("%d", &b);
+    printf ("Entre com b: \n");
+    scanf ("%d", &b);
 
-printf ("Invertendo:\nb= %d\na = %d\n", b, a);
-printf ("Ou invertendo o valor deles:\na= %d\nb = %d", b, a);
-return 0;
+    int temp = a;
+
+    a = b;
+    b = temp;
+
+    printf ("Invertendo valores...");
+    printf ("\na = %d\nb = %d", b, a);
+    return 0;
 }
